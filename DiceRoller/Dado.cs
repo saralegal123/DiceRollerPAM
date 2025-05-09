f629338b045bb5a551d14700941996f4db0c8cf3
Sara Marcely Andrade de Oliveira <saradomacario@gmail.com>
2025-05-09 20:06:37 -0300

    PAM - DiceRoller (programa para rolar dados)

---
+++ DiceRoller/Dado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class Dado
+    {
+        //ModificadorDeAcesso Tipo Nome
+        private string ladoSorteado;
+
+        public string LadoSorteado { get => ladoSorteado; set => ladoSorteado = value; }
+
+        public Dado()
+        {
+        }
+
+        public string Roller()
+        {
+            Random random = new Random();
+            if (random.Next(2) == 0)
+            {
+                LadoSorteado = "1";
+            }
+            else
+            {
+                LadoSorteado = "coroa";
+            }
+            //   LadoSorteado = random.Next(2) == 0 ? "Cara" : "Coroa";
+
+            return (LadoSorteado);
+        }
+}
